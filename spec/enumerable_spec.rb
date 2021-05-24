c041944bdea03e1fc6e3a340da7b499ea50f4adc
Ángel Díaz <diazangel9311@gmail.com>
2021-05-24 15:10:56 -0500

    Add multiply_els and fixed linter issues

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../enumerables'
 
 describe Enumerable do
-  describe 'my_each' do
+  describe '#my_each' do
     let(:check) { [1, 2, 3].my_each }
 
     it 'Iterates through a series of elements of an array' do
@@ -15,7 +15,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_each_with_index' do
+  describe '#my_each_with_index' do
     let(:hasx) { {} }
     let(:arr) { %w[Apple Cherry Orange] }
     let(:check) { arr.my_each_with_index }
@@ -34,7 +34,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_select' do
+  describe '#my_select' do
     let(:arr) { [10, 11, 12, 14] }
     let(:check) { arr.select(&:even?) }
 
@@ -52,7 +52,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_all?' do
+  describe '#my_all?' do
     it 'Returns true if all elements are odd' do
       expect([11, 13, 1, 7, 9].my_all?(&:odd?)).to eq(true)
     end
@@ -82,7 +82,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_any?' do
+  describe '#my_any?' do
     it 'Returns false if no element is odd' do
       expect([8, 2, 10, 14, 70].my_any?(&:odd?)).to eq(false)
     end
@@ -112,7 +112,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_none?' do
+  describe '#my_none?' do
     it 'Returns true if all elements are not odd' do
       expect([8, 2, 10, 14, 70].my_none?(&:odd?)).to eq(true)
     end
@@ -142,7 +142,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_count' do
+  describe '#my_count' do
     it 'Counts the elements in an array' do
       expect([1, 2, 3, 4, 5].my_count).to eq(5)
     end
@@ -163,7 +163,7 @@ describe Enumerable do
     end
   end
 
-  describe 'my_map' do
+  describe '#my_map' do
     it 'Returns an array with the product of each number by itself' do
       expect([1, 2, 3, 4].my_map { |i| i * i }).to eq([1, 4, 9, 16])
     end
@@ -171,11 +171,17 @@ describe Enumerable do
       expect((5..10).my_map { |i| i * i }).to eq([25, 36, 49, 64, 81, 100])
     end
     it 'Returns an array with upper case words' do
-      expect(%w[AppLe oRangE berrY].my_map { |i| i.upcase }).to eq(%w[APPLE ORANGE BERRY])
+      expect(%w[AppLe oRangE berrY].my_map(&:upcase)).to eq(%w[APPLE ORANGE BERRY])
+    end
+    it 'Returns error if no element provided' do
+      expect { my_map { |i| i * i } }.to raise_error(NoMethodError)
+    end
+    it 'Returns error if no elements can\'t be multiply' do
+      expect { %w[apple berry orange].my_map { |i| i * i } }.to raise_error(TypeError)
     end
   end
 
-  describe 'my_inject' do
+  describe '#my_inject' do
     it 'Adds up all the numbers in the range' do
       expect((5..10).my_inject(:+)).to eq(45)
     end
@@ -192,7 +198,25 @@ describe Enumerable do
       expect([15, 15].my_inject(2) { |i, value| i - value }).to eq(-28)
     end
     it 'Return the longest word' do
-      expect(%w{ cat sheep bear }.inject { |memo, word| memo.length > word.length ? memo : word }).to eq('sheep')
+      expect(%w[cat sheep bear].inject { |memo, word| memo.length > word.length ? memo : word }).to eq('sheep')
+    end
+    it 'Returns error if no element provided' do
+      expect { my_inject { |i| i * i } }.to raise_error(NoMethodError)
+    end
+    it 'Returns error if no elements can\'t be multiply' do
+      expect { %w[apple berry orange].my_inject { |i| i * i } }.to raise_error(TypeError)
+    end
+  end
+
+  describe '#multiply_els' do
+    it 'Multiplies all numbers in the array' do
+      expect(multiply_els([1, 2, 3])).to eq(6)
+    end
+    it 'Multiplies all numbers in the range' do
+      expect(multiply_els((1..3))).to eq(6)
+    end
+    it 'Returns error if no elements can\'t be multiply' do
+      expect { multiply_els(%w[apple berry sunday]) }.to raise_error(TypeError)
     end
   end
 end
