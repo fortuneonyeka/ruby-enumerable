26a9beefd1601951cb01198698cae2284fe90496
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-25 17:41:20 +0100

    create rspec files and variables for Enumerable tests

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,12 @@
+require './enumerables'
+
+
+describe Enumerable do
+  let(:collegues) {%w["Mike", "John", "Shaw", "Jane"]}
+  let(:array_strings) {["fan", "foul", "fond"]}
+  let(:range) {(1..9)}
+  let(:numbers) {[2, 5, 6, 9, 10, 13, 27, 50]}
+  let(:arry) {[1, 3, 2, 7, 8]}
+
+  
+end
