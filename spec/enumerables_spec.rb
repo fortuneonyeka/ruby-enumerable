5e7af8762950fffe4a9da9b3521d064a19ad462d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-25 20:35:22 +0100

    fix rubocop errors

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,112 +1,107 @@
 require './enumerables'
 
-
 describe Enumerable do
-  let(:collegues) {%w["Mike", "John", "Shaw", "Jane"]}
-  let(:array_strings) {["fan", "foul", "fond"]}
-  let(:range) {(1..9)}
-  let(:numbers) {[2, 5, 6, 9, 10, 13, 27, 50]}
-  let(:arry) {[1, 3, 2, 7, 8]}
+  let(:range) { (1..9) }
+  let(:arry) { [1, 3, 2, 7, 8] }
 
-  describe "my_each" do
-    it "returns an enumerator if no block or arguments is given" do
+  describe 'my_each' do
+    it 'returns an enumerator if no block or arguments is given' do
       expect([1, 3, 2, 7, 8].my_each).to be_a Enumerator
     end
-    it "returns an array when a block is given" do
-      expect([1, 3, 2, 7, 8].my_each{|x| x}).to eql(arry)
+    it 'returns an array when a block is given' do
+      expect([1, 3, 2, 7, 8].my_each { |x| x }).to eql(arry)
     end
   end
 
-describe "my_each_with_index" do
-  it "returns an enumerator if no block is given" do
-    expect([1, 3, 2, 7, 8].my_each_with_index).to be_a Enumerator
+  describe 'my_each_with_index' do
+    it 'returns an enumerator if no block is given' do
+      expect([1, 3, 2, 7, 8].my_each_with_index).to be_a Enumerator
+    end
+    it ' returns an array with index when block is given' do
+      expect([1, 3, 2, 7, 8].my_each_with_index { |x, _i| x }).to eql(arry)
+    end
   end
-  it " returns an array with index when block is given" do
-    expect([1, 3, 2, 7, 8].my_each_with_index{|x, i| x}).to eql(arry)
+  describe 'my_select' do
+    it 'returns an enumerator if no block is given' do
+      expect([1, 3, 2, 7, 8].my_select).to be_a Enumerator
+    end
+    it 'returns an array containing all elements to which the block given returns true' do
+      expect([1, 3, 2, 7, 8].my_select(&:even?)).to eql([2, 8])
+    end
   end
-end
-describe "my_select" do
-  it "returns an enumerator if no block is given" do
-    expect([1, 3, 2, 7, 8].my_select). to be_a Enumerator
-  end
-  it "returns an array containing all elements to which the block given returns true" do
-    expect([1, 3, 2, 7, 8].my_select(&:even?)).to eql([2, 8])
-  end
-end
 
-describe "my_all?" do
-  it "returns false when all provided argument are not of the same data type" do
-    expect([1, 3, 2, "home", 8].my_all?(Numeric)).to eql(false)
+  describe 'my_all?' do
+    it 'returns false when all provided argument are not of the same data type' do
+      expect([1, 3, 2, 'home', 8].my_all?(Numeric)).to eql(false)
+    end
+    it 'returns true when array containing all elements to which the block or argument given returns true' do
+      expect([1, 3, 2, 7, 8].my_all?(Numeric)).to eql(true)
+    end
+    it 'returns true when all the element in the array are string' do
+      expect(%w[fan foul fond].my_all?).to eq(true)
+    end
   end
-  it "returns true when array containing all elements to which the block or argument given returns true" do
-    expect([1, 3, 2, 7, 8]. my_all?(Numeric)).to eql(true)
-  end
-  it "returns true when all the element in the array are string" do
-    expect(["fan", "foul", "fond"].my_all?).to eq(true)
-  end
-end
 
-describe "any?" do
-  it "returns true when the object is not an array" do
-    result = range.my_any?{|x| x > 7}
-    expect(result).to eql(true)
-  end
-  it "when object is an array" do
-    result = range.my_any?{|x| x == 12}
-    expect(result).to eql(false)
-  end
-  end
-  describe "my_none?" do
-    it "returns true if the block never returns true" do
-      expect(["fan", "foul", "fond"].my_none?(Numeric)).to eql(true)
+  describe 'any?' do
+    it 'returns true when the object is not an array' do
+      result = range.my_any? { |x| x > 7 }
+      expect(result).to eql(true)
     end
-    it "returns true if regex pattern never matches with the elements" do
-      expect(["fan", "foul", "fond"].my_none?(/w/)).to eql(true)
+    it 'when object is an array' do
+      result = range.my_any? { |x| x == 12 }
+      expect(result).to eql(false)
     end
-    it "returns false if at least one of the element matches or of the same class" do
-      expect(["fan", "foul", 20, "fond"].my_none?(Numeric)).to eql(false)
+  end
+  describe 'my_none?' do
+    it 'returns true if the block never returns true' do
+      expect(%w[fan foul fond].my_none?(Numeric)).to eql(true)
     end
-    it "returns true if array is empty" do
+    it 'returns true if regex pattern never matches with the elements' do
+      expect(%w[fan foul fond].my_none?(/w/)).to eql(true)
+    end
+    it 'returns false if at least one of the element matches or of the same class' do
+      expect(['fan', 'foul', 20, 'fond'].my_none?(Numeric)).to eql(false)
+    end
+    it 'returns true if array is empty' do
       expect([].my_none?).to eql(true)
     end
-    it "returns true if an argument equals nil" do
+    it 'returns true if an argument equals nil' do
       expect([nil].my_none?).to eql(true)
     end
   end
 
-  describe "my_count" do
-    it "if block given" do
+  describe 'my_count' do
+    it 'if block given' do
       expect(arry.my_count).to eql(5)
     end
-    it "return even numbers when block is given" do
-      expect(arry.my_count(&:even?)).to eql(2)  
+    it 'return even numbers when block is given' do
+      expect(arry.my_count(&:even?)).to eql(2)
     end
-    it "when block given" do
-    result = arry.my_count{|x| x > 5}
-    expect(result).to eql(2)
+    it 'when block given' do
+      result = arry.my_count { |x| x > 5 }
+      expect(result).to eql(2)
     end
   end
 
-  describe "my_map" do
-    it "when block is given" do
-      result = range.map{|x| x * x}
+  describe 'my_map' do
+    it 'when block is given' do
+      result = range.map { |x| x * x }
       expect(result).to eql([1, 4, 9, 16, 25, 36, 49, 64, 81])
     end
-    it "returns Enumerator when no block and proc is given" do
+    it 'returns Enumerator when no block and proc is given' do
       expect(range.my_map).to be_a Enumerator
-    end 
+    end
   end
-  
-  describe "my_inject" do
-    it "when block given" do
-      result = (5..10).my_inject{|sum, n| sum + n}
-        expect(result).to eql(45)
-    end
-    it "when block has a method and an argument" do
-      result = (1..3).my_inject(1) {|x, i| x * i}
-      expected = (1..3).my_inject(1) {|x, i| x * i}
-      expect(result).to eql(expected) 
-    end
-  end 
 
+  describe 'my_inject' do
+    it 'when block given' do
+      result = (5..10).my_inject { |sum, n| sum + n }
+      expect(result).to eql(45)
+    end
+    it 'when block has a method and an argument' do
+      result = (1..3).my_inject(1) { |x, i| x * i }
+      expected = (1..3).my_inject(1) { |x, i| x * i }
+      expect(result).to eql(expected)
+    end
+  end
 end
